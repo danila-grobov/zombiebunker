597982a76b1f04901132fc2e3f4047d9fdf4b9a8
Danila Grobov <danila.grob@gmail.com>
2019-10-01 15:35:20 -0700

    Asdfadsf

--- wp-content/plugins/booking-calendar-dgrobov/includes/dbconnect.php
+++ wp-content/plugins/booking-calendar-dgrobov/includes/dbconnect.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
 //EDIT ONLY FOLLOWING 5 LINES
 $db_host = 'localhost'; //hostname
-$db_user = 'root'; // username
-$db_password = 'root'; // password
-$db_name = 'local'; //database name
+$db_user = 'zombie'; // username
+$db_password = 'katiakurmis'; // password
+$db_name = 'zombiebunker'; //database name
 $baseDir = '/wp-content/plugins/booking-calendar-dgrobov/'; // Don't change this variable if you will be using booking in the ROOT of the username. 
 // otherwise - change to $baseDir = "/directoryName/"; WITH TRAILING SLASH!
 $demo = false;
